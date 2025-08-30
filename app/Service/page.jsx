ed69181a421631d0d91c4206248835bb0bd2cd40
Fame hyder <famehyder9999@gmail.com>
2025-08-30 17:34:04 +0500

    UDB

--- app/Service/page.jsx
+++ app/Service/page.jsx
@@ -2,12 +2,17 @@ import React from 'react'
 import Service from '../../components/Service/Service1.jsx';
 import OurTech from '../../components/Service/OurTech.jsx';
 import Development from '../../components/Service/Development.jsx';
+import GetyourBusiness from '../../components/Home/getYourBussiness.jsx';
+
+
+
 const page = () => {
   return (
     <div>
       <Service />
       <OurTech />
       <Development />
+      <GetyourBusiness />
     </div>
   )
 }
